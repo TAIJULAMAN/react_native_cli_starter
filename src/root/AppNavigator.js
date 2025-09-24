41c848f9d32552e4e5cb6f6eb3c9341c90629dac
TAIJULAMAN <taijulaman8@gmail.com>
2025-09-25 01:08:03 +0600

    complete basic cli starter

--- src/root/AppNavigator.js
+++ src/root/AppNavigator.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import OnboardingScreen from '../screens/OnboardingScreen';
 import BottomNavigatorScreen from '../screens/bottomNavigator/BottomNavigatorScreen';
 const Stack = createNativeStackNavigator();
 const AppNavigator = () => {
   return (
- <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+    <Stack.Navigator initialRouteName="BottomNavigator" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="BottomNavigator" component={BottomNavigatorScreen} />
 
     </Stack.Navigator>
